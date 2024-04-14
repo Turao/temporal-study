eb00a29ee0479ec57546efeb7076934e743ee61d
turao <lenz.arthur@gmail.com>
2024-04-14 16:45:37 -0300

    mock and test activity (not working)

--- go.mod
+++ go.mod
@@ -4,7 +4,10 @@ go 1.20
 
 require (
 	github.com/gofrs/uuid v4.4.0+incompatible
+	github.com/stretchr/testify v1.9.0
+	go.temporal.io/api v1.29.1
 	go.temporal.io/sdk v1.26.0
+	go.uber.org/mock v0.4.0
 )
 
 require (
@@ -20,8 +23,6 @@ require (
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/robfig/cron v1.2.0 // indirect
 	github.com/stretchr/objx v0.5.2 // indirect
-	github.com/stretchr/testify v1.9.0 // indirect
-	go.temporal.io/api v1.29.1 // indirect
 	golang.org/x/exp v0.0.0-20231127185646-65229373498e // indirect
 	golang.org/x/net v0.22.0 // indirect
 	golang.org/x/sys v0.18.0 // indirect
---
+++ src/temporal/activities/upsert-project/activity_test.go
@@ -0,0 +1,82 @@
+package upsertproject
+
+import (
+	"context"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	mockservice "github.com/turao/temporal-study/mocks/src/service"
+	"github.com/turao/temporal-study/src/api"
+	"github.com/turao/temporal-study/src/service"
+	"go.temporal.io/sdk/testsuite"
+	"go.uber.org/mock/gomock"
+)
+
+func TestExecute(t *testing.T) {
+	tests := map[string]struct {
+		Context          context.Context
+		Request          Request
+		ExpectedResponse *Response
+		ExpectedError    error
+		// mocks
+		ProjectService func(t *testing.T) service.ProjectService
+	}{
+		"success": {
+			Context: context.Background(),
+			Request: Request{
+				Request: &api.UpsertProjectRequest{},
+			},
+			ExpectedResponse: &Response{
+				Response: &api.UpsertProjectResponse{},
+			},
+			ExpectedError: nil,
+			ProjectService: func(t *testing.T) service.ProjectService {
+				controller := gomock.NewController(t)
+				mock := mockservice.NewMockProjectService(controller)
+				mock.EXPECT().
+					UpsertProject(gomock.Any(), gomock.Any()).
+					Return(&api.UpsertProjectResponse{}, nil)
+				return mock
+			},
+		},
+		"error - upsert project failure": {
+			Context: context.Background(),
+			Request: Request{
+				Request: &api.UpsertProjectRequest{},
+			},
+			ExpectedResponse: nil,
+			ExpectedError:    assert.AnError,
+			ProjectService: func(t *testing.T) service.ProjectService {
+				controller := gomock.NewController(t)
+				mock := mockservice.NewMockProjectService(controller)
+				mock.EXPECT().
+					UpsertProject(gomock.Any(), gomock.Any()).
+					Return(nil, assert.AnError)
+				return mock
+			},
+		},
+	}
+
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			suite := testsuite.WorkflowTestSuite{}
+			env := suite.NewTestActivityEnvironment()
+
+			activity := Activity{
+				ProjectService: test.ProjectService(t),
+			}
+
+			env.RegisterActivity(activity.Execute)
+
+			future, err := env.ExecuteActivity(activity.Execute, test.Request)
+			assert.ErrorIs(t, test.ExpectedError, err)
+
+			var res *Response
+			if future != nil {
+				future.Get(&res)
+			}
+
+			assert.Equal(t, test.ExpectedResponse, res)
+		})
+	}
+}
