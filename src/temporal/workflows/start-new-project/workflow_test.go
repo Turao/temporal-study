71f0beb7c88c1142c129fdd9d9f874c7e238737b
turao <lenz.arthur@gmail.com>
2025-01-12 09:27:07 -0300

    fix workflow unit test

--- src/temporal/workflows/start-new-project/workflow_test.go
+++ src/temporal/workflows/start-new-project/workflow_test.go
@@ -1,23 +1,49 @@
 package startnewproject
 
 import (
+	"context"
 	"testing"
 
+	"github.com/gofrs/uuid"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
 	"github.com/stretchr/testify/require"
+	"github.com/turao/temporal-study/src/api"
+	"github.com/turao/temporal-study/src/entity/project"
 	"github.com/turao/temporal-study/src/temporal/activities"
 	createproject "github.com/turao/temporal-study/src/temporal/activities/create-project"
 	notifyprojectowner "github.com/turao/temporal-study/src/temporal/activities/notify-project-owner"
+	"go.temporal.io/sdk/activity"
 	"go.temporal.io/sdk/testsuite"
-	"go.uber.org/mock/gomock"
 )
 
 func TestExecute(t *testing.T) {
+	registerActivityStubs := func(env *testsuite.TestWorkflowEnvironment) {
+		env.RegisterActivityWithOptions(
+			func(ctx context.Context, req createproject.Request) (*createproject.Response, error) {
+				return nil, nil
+			},
+			activity.RegisterOptions{
+				Name: activities.ActivityNameCreateProject,
+			},
+		)
+
+		env.RegisterActivityWithOptions(
+			func(ctx context.Context, req notifyprojectowner.Request) (*notifyprojectowner.Response, error) {
+				return nil, nil
+			},
+			activity.RegisterOptions{
+				Name: activities.ActivityNameNotifyProjectOwner,
+			},
+		)
+	}
+
 	tests := map[string]struct {
 		Request          Request
 		ExpectedResponse *Response
 		ExpectedError    error
 		// mocks
+		MockTestWorkflowEnvironment func(t *testing.T) *testsuite.TestWorkflowEnvironment
 	}{
 		"success": {
 			Request: Request{
@@ -26,30 +52,40 @@ func TestExecute(t *testing.T) {
 			},
 			ExpectedResponse: &Response{},
 			ExpectedError:    nil,
+			MockTestWorkflowEnvironment: func(t *testing.T) *testsuite.TestWorkflowEnvironment {
+				suite := testsuite.WorkflowTestSuite{}
+				env := suite.NewTestWorkflowEnvironment()
+
+				registerActivityStubs(env)
+
+				env.OnActivity(activities.ActivityNameCreateProject, mock.Anything, mock.Anything).Return(
+					&createproject.Response{
+						Entity: &project.Project{
+							ID:      uuid.Must(uuid.NewV4()),
+							Name:    "project-name",
+							OwnerID: uuid.Must(uuid.NewV4()),
+						},
+					},
+					nil,
+				)
+
+				env.OnActivity(activities.ActivityNameNotifyProjectOwner, mock.Anything, mock.Anything).Return(
+					&notifyprojectowner.Response{
+						Response: &api.NotifyResponse{
+							NotificationID: uuid.Must(uuid.NewV4()).String(),
+						},
+					},
+					nil,
+				)
+
+				return env
+			},
 		},
 	}
 
 	for name, test := range tests {
 		t.Run(name, func(t *testing.T) {
-			suite := testsuite.WorkflowTestSuite{}
-			env := suite.NewTestWorkflowEnvironment()
-
-			// ! this does not work because RegisterActivity expects a function (or struct?)
-			// env.RegisterActivity(activities.ActivityNameCreateProject)
-			// env.RegisterActivity(activities.ActivityNameNotifyProjectOwner)
-
-			// ! this does not work because RegisterActivityWithOptions expects a internal struct (and we cannot use internal packages)
-			// env.RegisterActivityWithOptions(nil, internal.RegisterActivityOptions{
-			// 	Name: activities.ActivityNameCreateProject,
-			// })
-			// env.RegisterActivityWithOptions(nil, internal.RegisterActivityOptions{
-			// 	Name: activities.ActivityNameNotifyProjectOwner,
-			// })
-
-			// ! this does not work because OnActivity expects the Activity to be registered (even though the registered function does not get invoked)
-			// ! see https://github.com/temporalio/sdk-go/issues/982
-			env.OnActivity(activities.ActivityNameCreateProject, gomock.Any()).Return(&createproject.Response{}, nil)
-			env.OnActivity(activities.ActivityNameNotifyProjectOwner, gomock.Any()).Return(&notifyprojectowner.Response{}, nil)
+			env := test.MockTestWorkflowEnvironment(t)
 
 			workflow := Workflow{}
 			env.ExecuteWorkflow(workflow.Execute, test.Request)
@@ -61,7 +97,6 @@ func TestExecute(t *testing.T) {
 			require.NoError(t, err)
 
 			assert.Equal(t, test.ExpectedResponse, res)
-
 		})
 	}
 }
